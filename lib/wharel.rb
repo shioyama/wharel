baac48a318cbbb202c7b17a2d65613e687db49f8
Chris Salzberg <chris@dejimata.com>
2018-05-25 13:06:26 +0900

    Add code

--- lib/wharel.rb
+++ lib/wharel.rb
@@ -1,5 +1,32 @@
 require "wharel/version"
+require "active_record"
 
 module Wharel
-  # Your code goes here...
+  module QueryMethods
+    def where(opts = :chain, *rest, &block)
+      block_given? ? super(VirtualRow.build_query(self, &block)) : super
+    end
+  end
+
+  class VirtualRow < BasicObject
+    def initialize(klass)
+      @klass = klass
+    end
+
+    def method_missing(m, *)
+      @klass.column_names.include?(m.to_s) ? @klass.arel_table[m] : super
+    end
+
+    class << self
+      def build_query(klass, &block)
+        row = new(klass)
+        query = (block.arity == 0) ? row.instance_eval(&block) : block.call(row)
+        ::ActiveRecord::Relation === query ? query.arel.constraints.inject(&:and) : query
+      end
+    end
+  end
 end
+
+# Monkey-patch ActiveRecord. Should make this optional.
+::ActiveRecord::Base.singleton_class.prepend ::Wharel::QueryMethods
+::ActiveRecord::Relation.prepend ::Wharel::QueryMethods
